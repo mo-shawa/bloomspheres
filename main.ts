7cf5a325e3fa742a8cb0a822b1cd9f4c56b6d3f5
moshawa <moshawa0@gmail.com>
2024-03-05 10:48:27 -0500

    bloom rendering

--- main.ts
+++ main.ts
@@ -1,7 +1,18 @@
 import './style.css'
 
+import { gen } from 'culler'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
+
+import { isMesh, isStandardMaterial } from './utils'
+
+const params = {
+	count: 100,
+	size: 0.1,
+}
 
 const canvas = document.querySelector<HTMLCanvasElement>('canvas.webgl')
 if (!canvas) throw new Error('Canvas not found')
@@ -37,22 +48,59 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setClearColor(new THREE.Color('#262837'))
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+const effectComposer = new EffectComposer(renderer)
+
+const bloomPass = new UnrealBloomPass()
+
+const renderPass = new RenderPass(scene, camera)
+
+effectComposer.addPass(renderPass)
+
+effectComposer.addPass(bloomPass)
 
 const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
-const testMesh = new THREE.Mesh(
-	new THREE.BoxGeometry(1, 1, 1),
-	new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
+function initScene() {
+	scene.traverse(disposeMaterial)
 
-scene.add(testMesh)
+	const geometry = new THREE.IcosahedronGeometry(1, 15)
+
+	for (let i = 0; i < params.count; i++) {
+		const material = new THREE.MeshBasicMaterial({
+			color: new THREE.Color(gen()),
+			wireframe: true,
+		})
+
+		const mesh = new THREE.Mesh(geometry, material)
+
+		mesh.position.x = (Math.random() - 0.5) * 4
+		mesh.position.y = (Math.random() - 0.5) * 4
+		mesh.position.z = (Math.random() - 0.5) * 4
+
+		mesh.scale.setScalar(Math.random() * params.size)
+
+		scene.add(mesh)
+	}
+}
+
+initScene()
 
 function tick() {
 	controls.update()
 
-	renderer.render(scene, camera)
+	// renderer.render(scene, camera)
+	effectComposer.render()
 
 	requestAnimationFrame(tick)
 }
 
 tick()
+
+function disposeMaterial(obj: THREE.Object3D) {
+	if (isMesh(obj) && isStandardMaterial(obj.material)) {
+		obj.material.dispose()
+	}
+}
